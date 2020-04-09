8dfc86f7afd239100dc23c635098d599cb3f0a24
Reilly Beacom <contact@reillybeacom.net>
2020-04-09 23:14:50 +1000

    Serialise photo download script and add a delay to avoid being throttled/banned by wikimedia.org

--- scripts/photos.js
+++ scripts/photos.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 
 const FS = require('fs');
 const Glob = require('glob');
@@ -14,156 +16,158 @@ const { allPhotosInDocument } = require('../utils/allPhotosInDocument');
 
 const regenerate = process.argv.indexOf('-r') !== -1;
 
-const indexFilePath = 'public/photos/index.json';
+const photosDirPath = 'public/photos';
+const indexFilePath = Path.join(photosDirPath, 'index.json');
 
-const index = {};
-
-const addSrc = (src, page) => {
-    if (index[src] === undefined) index[src] = {};
-    if (index[src].pages === undefined) index[src].pages = [];
-    index[src].pages.push(page);
-    index[src].hash = crypto
-        .createHash('md5')
-        .update(src)
-        .digest('hex');
-    index[src].ext = Path.extname(src).toLowerCase();
-};
-
-const sitePhotos = JSON.parse(FS.readFileSync('utils/sitePhotos.json'));
-
-Object.values(sitePhotos).forEach(({ src }) => addSrc(src, 'site'));
-
-console.log('Loading YAML:');
-
-['attractions', 'campsites', 'parks', 'routes'].forEach(category => {
-    Glob.sync(`public/docs/${category}/**/*.yaml`).forEach(fpath => {
-        console.log(' ', fpath);
-        const doc = YAML.safeLoad(FS.readFileSync(fpath));
-        const photos = allPhotosInDocument(doc);
-        // console.log(photos);
-        photos.forEach(({ src }) => addSrc(src, fpath.replace('public/docs', '')));
+// https://stackoverflow.com/a/51302466/5165
+const download = (src, filePath) =>
+    new Promise(resolve => {
+        console.log('Fetch...');
+        if (FS.existsSync(filePath) && !regenerate) {
+            console.log(' > EXISTS');
+            resolve(true);
+        } else {
+            console.log('DOWNLOADING...');
+            fetch(src)
+                .then(response => {
+                    if (!response.ok) {
+                        console.log(` > ERROR ${response.status}`);
+                        resolve(false);
+                    } else {
+                        const fileStream = FS.createWriteStream(filePath);
+                        response.body.pipe(fileStream);
+                        response.body.on('error', () => {
+                            console.log(` > FAILED!`);
+                            resolve(false);
+                        });
+                        fileStream.on('finish', () => {
+                            console.log(` > OK`);
+                            if (src.indexOf('wikipedia') !== -1) {
+                                setTimeout(() => resolve(true), 1000);
+                            } else {
+                                resolve(true);
+                            }
+                        });
+                    }
+                })
+                .catch(() => {
+                    console.log(` > ERROR`);
+                    resolve(false);
+                });
+        }
     });
-});
 
-const cacheFilePath = url => Path.join('public/photos/orig', index[url].hash) + index[url].ext;
-
-const resizedFilePath = (url, dir) => `${Path.join('public/photos', dir, index[url].hash)}.jpg`;
+const resize = (inFilePath, outFilePath, width, height) =>
+    new Promise(resolve => {
+        console.log(`Resize ${width}x${height}...`);
+        if (FS.existsSync(outFilePath) && !regenerate) {
+            console.log(' > EXISTS');
+            resolve();
+        } else {
+            // https://sharp.pixelplumbing.com/api-resize
+            Sharp(inFilePath)
+                .resize({
+                    width,
+                    height,
+                    fit: Sharp.fit.inside,
+                })
+                .toFormat('jpg')
+                .toFile(outFilePath, err => {
+                    console.log(` > ${err ? 'ERROR' : 'OK'}`);
+                    resolve();
+                });
+        }
+    });
 
 const dmsToDecimal = ([d, m, s]) => (d + m / 60 + s / 3600).toFixed(6);
 
-const extractExif = url =>
+const extractExif = (filePath, obj) =>
     new Promise(resolve => {
-        // TODO: Also extract license info: https://wiki.creativecommons.org/wiki/XMP
-        if (regenerate || index[url].exif === undefined) {
+        console.log('EXIF...');
+        if (obj.exif !== undefined && !regenerate) {
+            console.log(` > ${obj.exif}`);
+            resolve();
+        } else {
             /* eslint-disable no-new */
             new ExifImage(
                 {
-                    image: cacheFilePath(url),
+                    image: filePath,
                 },
                 (error, data) => {
                     if (error) {
-                        index[url].exif = false;
+                        obj.exif = false;
                     } else {
+                        obj.exif = true;
                         if ('exif' in data && 'DateTimeOriginal' in data.exif) {
-                            index[url].year = moment(
+                            obj.year = moment(
                                 data.exif.DateTimeOriginal,
                                 'YYYY:MM:DD HH:mm:ss',
                             ).year();
+                            console.log(` > ${obj.year}`);
                         }
                         if ('gps' in data && 'GPSLatitude' in data.gps) {
-                            index[url].location = [
+                            obj.location = [
                                 dmsToDecimal(data.gps.GPSLatitude) *
                                     (data.gps.GPSLatitudeRef === 'N' ? 1 : -1),
                                 dmsToDecimal(data.gps.GPSLongitude) *
                                     (data.gps.GPSLongitudeRef === 'E' ? 1 : -1),
                             ];
-                            console.log(`LOCATION: ${JSON.stringify(index[url].location)}`);
+                            console.log(` > ${obj.location[0]},${obj.location[0]}`);
                         }
-                        index[url].exif = true;
                     }
+                    console.log(` > ${obj.exif}`);
                     resolve();
                 },
             );
             /* eslint-enable no-new */
-        } else {
-            resolve();
         }
     });
 
-const resizeFile = (url, dir, options) =>
-    new Promise(resolve => {
-        const path = cacheFilePath(url);
-        const outFile = resizedFilePath(url, dir);
-        if (regenerate || !FS.existsSync(outFile)) {
-            // https://sharp.pixelplumbing.com/api-resize
-            Sharp(path)
-                .resize(options)
-                .toFormat('jpg')
-                .toFile(outFile, err => {
-                    if (err) {
-                        console.log(`RESIZE FAILED! (${dir}): ${url}`);
-                        console.log('  ', err);
-                    } else {
-                        console.log(`RESIZE OK (${dir}): ${url}`);
-                    }
-                    resolve();
-                });
-        } else {
-            resolve();
+const run = async index => {
+    const addSrc = async (src, page) => {
+        console.log(src);
+        const obj = index[src] === undefined ? {} : index[src];
+        if (obj.pages === undefined) obj.pages = [];
+        if (obj.pages.indexOf(page) === -1) {
+            obj.pages.push(page);
         }
-    });
-
-// https://stackoverflow.com/a/51302466/5165
-const downloadFile = async url => {
-    const path = cacheFilePath(url);
-    if (regenerate || !FS.existsSync(path)) {
-        console.log(`DOWNLOADING: ${url}`);
-        const response = await fetch(url);
-        if (response.ok) {
-            let ok = false;
-            const fileStream = FS.createWriteStream(path);
-            await new Promise((resolve, reject) => {
-                response.body.pipe(fileStream);
-                response.body.on('error', err => {
-                    console.log(`DOWNLOAD FAILED! ${url}`);
-                    reject(err);
-                });
-                fileStream.on('finish', () => {
-                    console.log(`DOWNLOAD OK: ${url}`);
-                    ok = true;
-                    resolve();
-                });
-            });
-            return ok;
-        } else {
-            console.log(`DOWNLOAD ERROR! (${response.status}) ${url}`);
-            return false;
+        obj.hash = crypto
+            .createHash('md5')
+            .update(src)
+            .digest('hex');
+        obj.ext = Path.extname(src).toLowerCase();
+        const filePath = Path.join(photosDirPath, 'orig', obj.hash) + obj.ext;
+        obj.ok = await download(src, filePath);
+        if (obj.ok) {
+            await resize(filePath, `${Path.join(photosDirPath, 'sm', obj.hash)}.jpg`, 640, 480);
+            await resize(filePath, `${Path.join(photosDirPath, 'lg', obj.hash)}.jpg`, 1400, 800);
+            await extractExif(filePath, obj);
         }
-    } else {
-        console.log(`EXISTS: ${url}`);
+        index[src] = obj;
         return true;
+    };
+
+    console.log('Site photos:');
+
+    for (const { src } of Object.values(JSON.parse(FS.readFileSync('utils/sitePhotos.json')))) {
+        await addSrc(src, 'sitePhotos');
     }
+
+    console.log('Loading YAML:');
+
+    for (const category of ['attractions', 'campsites', 'parks', 'routes']) {
+        for (const filePath of Glob.sync(`public/docs/${category}/**/*.yaml`)) {
+            console.log(filePath);
+            const photos = allPhotosInDocument(YAML.safeLoad(FS.readFileSync(filePath)));
+            for (const { src } of photos) {
+                await addSrc(src, filePath.replace('public/docs', ''));
+            }
+        }
+    }
+
+    return index;
 };
 
-const processFile = async url => {
-    if (await downloadFile(url)) {
-        await Promise.all([
-            extractExif(url),
-            resizeFile(url, 'sm', {
-                width: 640,
-                height: 480,
-                fit: Sharp.fit.inside,
-            }),
-            resizeFile(url, 'lg', {
-                width: 1400,
-                height: 800,
-                fit: Sharp.fit.inside,
-            }),
-        ]);
-    }
-};
-
-Promise.all(Object.keys(index).map(url => processFile(url))).then(() => {
-    console.log('Done!');
+run(FS.existsSync(indexFilePath) ? JSON.parse(FS.readFileSync(indexFilePath)) : {}).then(index => {
     FS.writeFileSync(indexFilePath, JSON.stringify(index, null, 4));
 });
